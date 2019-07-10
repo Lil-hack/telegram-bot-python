d1a3f5cef04a64869820fc5fd63c5947e557aefc
Zikboy <43441041+akashkinKV@users.noreply.github.com>
2019-07-10 15:57:27 +0000

    try2

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import logging
 import os
 from aiogram import Bot, types, md
-from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InputMediaPhoto, InputMediaDocument
+import time
+from urllib.request import urlopen
+import json
 
 TOKEN = '873656324:AAFqF5d_0oAMgN2F2XPW5xMjrGULZvUnZTI'
 
@@ -33,7 +37,29 @@ async def welcome(message: types.Message):
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await bot.send_message(message.chat.id, message.text)
+    with open('data.json', 'rb') as f:
+        print(f)
+        await bot.send_document(message.chat.id, f)
+
+    t0 = time.time()
+
+    json2 = await bot.forward_message(message.chat.id, message.chat.id, message.message_id + 1)
+    data = await bot.get_file(json2.document.file_id)
+    data2 = bot.get_file_url(data.file_path)
+
+    page_source = urlopen(data2).read()
+    print(page_source)
+    d = json.loads(page_source)
+
+    with open('data3.json', 'w') as json_file:
+        json.dump(d, json_file)
+
+    with open('data3.json', 'rb') as f:
+        await bot.edit_message_media(InputMediaDocument(f), message.chat.id, message.message_id + 1)
+
+    t1 = time.time()
+
+    print(t1 - t0)
 
 
 async def on_startup(dp):
