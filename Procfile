f53a58f8f026b4432b107286f43b4a3e8741c17c
Zikboy <43441041+akashkinKV@users.noreply.github.com>
2019-07-10 15:50:57 +0000

    new

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn run_server:server
+web: python main.py
---
+++ main.py
@@ -0,0 +1,51 @@
+import logging
+import os
+from aiogram import Bot, types, md
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
+
+TOKEN = '873656324:AAFqF5d_0oAMgN2F2XPW5xMjrGULZvUnZTI'
+
+
+WEBHOOK_HOST = 'https://deploy-heroku-bot.herokuapp.com'  # name your app
+WEBHOOK_PATH = '/webhook/'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.environ.get('PORT')
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands='start')
+async def welcome(message: types.Message):
+    await bot.send_message(
+        message.chat.id,
+        f'Приветствую! Это демонтрационный бот\n'
+        f'Подробная информация на '
+        f'{md.hlink("github", "https://github.com/deploy-your-bot-everywhere/heroku")}',
+        parse_mode=types.ParseMode.HTML,
+        disable_web_page_preview=True)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await bot.send_message(message.chat.id, message.text)
+
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+
+
+async def on_shutdown(dp):
+    # insert code here to run it before shutdown
+    pass
+
+
+if __name__ == '__main__':
+    start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
+                  on_startup=on_startup, on_shutdown=on_shutdown,
+                  host=WEBAPP_HOST, port=WEBAPP_PORT)
