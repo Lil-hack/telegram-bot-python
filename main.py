5159ffc0fc38a50497f346ff12017471a97822a6
akashkin <zik1554@bk.ru>
2019-07-15 15:57:49 +0300

    timer2

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ async def on_shutdown(dp):
 
 if __name__ == '__main__':
     printit()
-    # start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
-    #               on_startup=on_startup, on_shutdown=on_shutdown,
-    #               host=WEBAPP_HOST, port=WEBAPP_PORT)
-    executor.start_polling(dp, skip_updates=True)
+    start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
+                  on_startup=on_startup, on_shutdown=on_shutdown,
+                  host=WEBAPP_HOST, port=WEBAPP_PORT)
+    # executor.start_polling(dp, skip_updates=True)
