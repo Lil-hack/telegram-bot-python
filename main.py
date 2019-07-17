2e4652f1c57846bbb7f31ddcdb51ef6c366995bb
Zikboy <43441041+akashkinKV@users.noreply.github.com>
2019-07-17 15:35:25 +0000

    timer work

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
 import os
+import re
+
 from aiogram import Bot, types, md
 from aiogram.utils.executor import start_webhook
 from aiogram import Bot, Dispatcher, executor, types
@@ -11,49 +13,14 @@ import threading
 import asyncio
 
 import math, time
-from async_cron.job import CronJob
-from async_cron.schedule import Scheduler
+import datetime
 
+from tornado import concurrent
 from twilio.rest import Client
 
 from twilio.rest import Client
 
 
-
-async def test(*args, **kwargs):
-    print(args, kwargs)
-
-
-def tt(*args, **kwargs):
-    print(args, kwargs)
-
-
-
-async def async_function():
-    print("Hello, World!")
-
-
-
-def printit():
-
-  json2 =   bot.forward_message(admin_id, admin_id, 4)
-  # data =  bot.get_file(json2.document.file_id)
-  # data2 = bot.get_file_url(data.file_path)
-  # page_source = urlopen(data2).read()
-  # d = json.loads(page_source)
-  # for user in d['users']:
-  #
-  #   print(user['chatid'])
-
-  # client = Client(account_sid, auth_token)
-  # call = client.calls.create(
-  #     url='https://ex.ru',
-  #     to='+79162721765',
-  #     from_='+12027967603'
-  # )
-  # print(call.sid)
-
-  print ("Hello, World!")
 
 account_sid = 'AC52a194acef951b3b36e94f294d836ae6'
 auth_token = '988090f0870502e26899be8b5aeb41f0'
@@ -75,6 +42,73 @@ dp = Dispatcher(bot)
 admin_id=852450369
 
 
+
+async def forward():
+    hour = datetime.datetime.now().time().hour
+    minute = datetime.datetime.now().time().minute
+
+    dataJson=await bot.forward_message(admin_id, admin_id, 4)
+    data = await bot.get_file(dataJson.document.file_id)
+    data2 = bot.get_file_url(data.file_path)
+    page_source = urlopen(data2).read()
+    d = json.loads(page_source)
+    for user in d['users']:
+        for time in user['calltime']:
+
+            if hour==time[0] and minute==time[1]:
+                client = Client(account_sid, auth_token)
+                call = client.calls.create(
+                url='https://ex.ru',
+                to='+79162721765',
+                from_='+12027967603'
+                )
+
+                # call = client.calls.create(
+                #     url='https://ex.ru',
+                #     to='+79167105584',
+                #     from_='+12027967603'
+                # )
+                #
+                # call = client.calls.create(
+                #     url='https://ex.ru',
+                #     to='+79171673630',
+                #     from_='+12027967603'
+                # )
+
+                user['calltime'].remove(time)
+            # if hour > time[0]:
+            #     user['calltime'].remove(time)
+
+    with open('data3.json', 'w') as json_file:
+        json.dump(d, json_file)
+    with open('data3.json', 'rb') as f:
+        await bot.edit_message_media(InputMediaDocument(f), admin_id, 4)
+
+
+def printit():
+    threading.Timer(60.0, printit).start()
+
+    try:
+        asyncio.run_coroutine_threadsafe(forward(),bot.loop)
+
+    except asyncio.TimeoutError:
+        print('The coroutine took too long, cancelling the task...')
+
+    except Exception as exc:
+        print(f'The coroutine raised an exception: {exc!r}')
+    else:
+        print(f'The coroutine returnr')
+
+
+  # client = Client(account_sid, auth_token)
+  # call = client.calls.create(
+  #     url='https://ex.ru',
+  #     to='+79162721765',
+  #     from_='+12027967603'
+  # )
+  # print(call.sid)
+
+
 @dp.message_handler(commands='start')
 async def welcome(message: types.Message):
     await bot.send_message(
@@ -91,15 +125,21 @@ async def echo(message: types.Message):
     # with open('data.json', 'rb') as f:
     #     print(f)
     #     await bot.send_document(message.chat.id, f)
+    json2 = await bot.forward_message(admin_id, admin_id, 4)
+    data = await bot.get_file(json2.document.file_id)
+    data2 = bot.get_file_url(data.file_path)
+    page_source = urlopen(data2).read()
+
+    if message.text == 'clean':
+        t0 = time.time()
+        with open('data2.json', 'rb') as f:
+            await bot.edit_message_media(InputMediaDocument(f), admin_id, 4)
+        t1 = time.time()
+        print(t1 - t0)
 
 
     if message.text=='tel':
         t0 = time.time()
-        json2 = await bot.forward_message(admin_id, admin_id, 4)
-        data = await bot.get_file(json2.document.file_id)
-        data2 = bot.get_file_url(data.file_path)
-        page_source = urlopen(data2).read()
-
 
         metka = False
         d = json.loads(page_source)
@@ -114,7 +154,8 @@ async def echo(message: types.Message):
             d['users'].append({'chatid': message.chat.id,
                                'phones': 8917,
                                'state': 0,
-                               'calltime': ['jd@example.com', 'jd@example.org']})
+                               'calltime': [[datetime.datetime.now().time().hour,datetime.datetime.now().time().minute+1]]})
+            print(str(datetime.datetime.now().time))
             await bot.send_message(message.chat.id, 'You append')
 
             with open('data3.json', 'w') as json_file:
@@ -124,14 +165,13 @@ async def echo(message: types.Message):
 
         t1 = time.time()
         print(t1 - t0)
+    try:
+        time_user=int(re.search(r'\d+', message.text).group())
+    except Exception:
+        time_user=0
 
-    else:
+    if  time_user > 0:
 
-        json2 = await bot.forward_message(admin_id, admin_id, 4)
-        data = await bot.get_file(json2.document.file_id)
-        data2 = bot.get_file_url(data.file_path)
-        page_source = urlopen(data2).read()
-        #
         d = json.loads(page_source)
         # for number in range(100):
         #     d['users'].append({'chatid': number,
@@ -147,17 +187,14 @@ async def echo(message: types.Message):
         t0 = time.time()
         for user in d['users']:
             if user['chatid'] == message.chat.id:
-                await bot.send_message(message.chat.id, 'You here')
-                print(user)
-                user['phones'] = 2
+                new_time=datetime.datetime.now()+datetime.timedelta(minutes=time_user)
+                user['calltime'].append([new_time.time().hour,new_time.time().minute])
                 with open('data3.json', 'w') as json_file:
                     json.dump(d, json_file)
                 with open('data3.json', 'rb') as f:
                     await bot.edit_message_media(InputMediaDocument(f), admin_id, 4)
 
                 break
-
-
 
 
         t1 = time.time()
@@ -174,21 +211,11 @@ async def on_shutdown(dp):
 
 
 if __name__ == '__main__':
-    # threading.Timer(5.0, printit).start()
-    msh = Scheduler()
-    myjob = CronJob(name='test', run_total=100).every(
-        5).second.go(test, (1, 2, 3), name=123)
-
-    msh.add_job(myjob)
+    printit()
 
 
     start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH,
                   on_startup=on_startup, on_shutdown=on_shutdown,
                   host=WEBAPP_HOST, port=WEBAPP_PORT)
-    loop = asyncio.get_event_loop()
 
-    try:
-        loop.run_until_complete(msh.start())
-    except KeyboardInterrupt:
-        print('exit')
     # executor.start_polling(dp, skip_updates=True)
